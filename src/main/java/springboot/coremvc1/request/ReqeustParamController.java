db07429c063f81526dce4121a6bcc2493900a7ed
br12345678 <mystyle730@gmail.com>
2023-01-17 23:31:05 +0900

    HTTP 요청 파라미터 - @RequestParam

--- src/main/java/springboot/coremvc1/request/ReqeustParamController.java
+++ src/main/java/springboot/coremvc1/request/ReqeustParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,66 @@ public class ReqeustParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody //이렇게 하면 String으로 "ok"라는 문자를 http응답 메세지에 넣어서 그대로 반환한다. RestController랑 같은 기능이다.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ) {
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(//RequestParam의 required의 기본값은 true이다.
+            @RequestParam(required = true) String username,//required = true인데 username을 안보내면 요청파라미터 스펙이 안맞아서 bad request를 내려준다.
+            @RequestParam(required = false) Integer age) {
+        //int는 null값을 넣을 수 없지만 Integer는 null값을 넣을 수 있다.
+        //username에 ""빈문자가 들어오면 필수값이 통과가 된다.
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(defaultValue = "guest") String username,
+            @RequestParam(defaultValue = "-1") int age) {
+        //defaultValue는 값이 있건 없건 default값이 들어가게 된다.
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+
+        return "ok";
     }
 }
